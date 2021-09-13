2a582fd105e29ac22406abcff7a18ba2e1215467
proguy914629 <gilbert@pt-gss.com>
2021-09-13 18:11:09 +0700

    Added a convert command
    Co-authored-by: whyfai <whyfai@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -191,6 +191,10 @@ async def table(ctx):
 
     await ctx.send(embed=embed)
 
+@bot.command()
+async def convert(ctx, num:int):
+    return await ctx.reply(f"{num} in roman numeral is {write_roman(num)}!\n\nFor the full table run `rc?table`")
+
 bot.load_extension('jishaku')
 
 bot.run(TOKEN)
